bc20de4ec573be57e8e809b593fdddcd44d3fd31
James Sinkala <xinnks@gmail.com>
2020-02-10 20:49:09 +0300

    add automatic load on browser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import XnsSeekBar from "./XnsSeekBar.vue";
 
-export default {
+const seeker = {
     install(Vue, options = {}) {
         // register component globally
         // https://vuejs.org/v2/guide/components-registration.html
         Vue.component("xns-seek-bar", XnsSeekBar);
     }
+}
+
+export default seeker
+
+if(typeof window !== 'undefined' && window.Vue){
+	window.Vue.use(seeker)
 }
